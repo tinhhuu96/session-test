0031fe3d62c5d2d7df4bfffff9dfc06a72c13293
tinhnh <tinhnh@runsystem.net>
2018-06-22 00:46:23 +0700

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Devise for User
+  devise_for :users, :controllers => {
+    confirmations: 'users/confirmations',
+    # omniauth_callbacks: 'users/omniauth_callbacks',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    unlocks: 'users/unlocks',
+  }, skip: [:sessions]
+  as :user do
+    get "login", to: "users/sessions#new", as: :new_user_session
+    post "login", to: "users/sessions#create", as: :user_session
+    delete "logout", to: "users/sessions#destroy", as: :destroy_user_session
+    get "users", to: "users#index", as: :users
+    get "users/:id", to: "users#show", as: :users_show
+  end
 end
